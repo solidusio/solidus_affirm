dd5871e59b1bf759b3e0f5ca72eb71ff4daeec18
Peter Berkenbosch <peter@stembolt.com>
2016-12-01 12:49:56 +0100

    Make sure we get the payload in json format

--- spec/controllers/spree/api/affirm_controller_spec.rb
+++ spec/controllers/spree/api/affirm_controller_spec.rb
@@ -3,9 +3,15 @@ require 'spec_helper'
 module Spree
   describe Api::AffirmController, type: :controller do
     render_views
+
+    before do
+      stub_authentication!
+    end
+
     describe "#payload" do
       it "renders the correct Affirm json payload" do
-        get :payload
+        get :payload, format: :json
+        expect(response).to be_success
       end
     end
   end
