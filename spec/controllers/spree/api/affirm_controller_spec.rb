a5d5a261353942845ed8fe9adceea5345088503c
Peter Berkenbosch <peter@stembolt.com>
2016-12-01 13:28:22 +0100

    make sure we setup an order to be rendered in the controller spec

--- spec/controllers/spree/api/affirm_controller_spec.rb
+++ spec/controllers/spree/api/affirm_controller_spec.rb
@@ -4,11 +4,17 @@ module Spree
   describe Api::AffirmController, type: :controller do
     render_views
 
+    let(:user) { create(:user) }
+    let(:current_api_user) { user }
+    let(:order) { create(:order_ready_to_complete, user: user) }
+
     before do
       stub_authentication!
     end
 
     describe "#payload" do
+      before { allow(controller).to receive(:current_order) { order } }
+
       it "renders the correct Affirm json payload" do
         get :payload, format: :json
         expect(response).to be_success
