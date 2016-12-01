e5ec546e88ebadebb9b967349fa1c6ef61915c53
Peter Berkenbosch <peter@stembolt.com>
2016-12-01 12:33:19 +0100

    include the needed helpers for getting the current_order

--- app/controllers/spree/api/affirm_controller.rb
+++ app/controllers/spree/api/affirm_controller.rb
@@ -1,7 +1,11 @@
 module Spree
   module Api
     class AffirmController < Spree::Api::BaseController
+      include Spree::Core::ControllerHelpers::Order
+      include Spree::Core::ControllerHelpers::Auth
+
       def payload
+        @order = current_order
       end
     end
   end
