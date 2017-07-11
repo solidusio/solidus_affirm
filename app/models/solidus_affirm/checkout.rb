c56a573361ca87a5d6b34e5de91d3d54c02d6781
Luuk Veenis <luuk@stembolt.com>
2017-07-11 09:22:39 -0700

    Make Affirm checkouts non-reusable
    This commit into Solidus core:
    https://github.com/solidusio/solidus/commit/547d459c26b2f48bbe2a75b58c90063f678fced2
    
    Introduced a check if payment sources are `reusable?`. The Checkout
    model here is used as the source, but doesn't implement that method,
    which causes any code that calls this to throw a method missing error.
    In particular I've seen this occur on the /admin/payments/new page.

--- app/models/solidus_affirm/checkout.rb
+++ app/models/solidus_affirm/checkout.rb
@@ -2,6 +2,10 @@ module SolidusAffirm
   class Checkout < ActiveRecord::Base
     self.table_name = "affirm_checkouts"
 
+    def reusable?
+      false
+    end
+
     def actions
       %w(capture void credit)
     end
--- spec/models/solidus_affirm/checkout_spec.rb
+++ spec/models/solidus_affirm/checkout_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 RSpec.describe SolidusAffirm::Checkout do
   subject { SolidusAffirm::Checkout.new }
 
+  describe "#reusable?" do
+    it "is never reusable" do
+      expect(subject.reusable?).to eq(false)
+    end
+  end
+
   it "supports the capture, void and credit actions" do
     expect(subject.actions).to eql ["capture", "void", "credit"]
   end
