5aaea0312f28261c97a47e926995fe578b18b4f1
Alberto Vena <kennyadsl@gmail.com>
2019-01-03 11:18:19 +0100

    Move solidus_support require before the engine
    This allows to use solidus support stuff into the engine file.

--- lib/solidus_affirm.rb
+++ lib/solidus_affirm.rb
@@ -1,5 +1,5 @@
 require 'solidus_core'
+require "solidus_support"
 require 'solidus_affirm/engine'
 require 'solidus_affirm/callback_hook/base'
 require 'solidus_affirm/affirm_client'
-require "solidus_support"
