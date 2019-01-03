9b11b08d53eb680ac9cf4ecbcf00f39f41103ce8
Alberto Vena <kennyadsl@gmail.com>
2019-01-03 11:17:35 +0100

    Add missing factories for affirm payments

---
+++ lib/solidus_affirm/factories/affirm_payment_factory.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :affirm_payment, class: Spree::Payment do
+    source_type "SolidusAffirm::Checkout"
+    state 'checkout'
+  end
+
+  factory :captured_affirm_payment, class: Spree::Payment do
+    payment_method { create(:affirm_payment_gateway) }
+    source { create(:affirm_checkout) }
+    response_code "123456789"
+  end
+end
